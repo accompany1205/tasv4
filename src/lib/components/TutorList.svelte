a9e6bb98de1c8b3a77781fb99e93461030d07f0c
accompany1205 <accompaniedwithyou@gmail.com>
2023-09-28 15:18:31 -0400

    Refactored images and calling through ids

--- src/lib/components/TutorList.svelte
+++ src/lib/components/TutorList.svelte
@@ -10,24 +10,18 @@
 
 <div class="m-auto flex justify-center gap-6 w-full flex-wrap">
   <!-- Tutors -->
-  {#each tutors as { name, title, hourlyRate }}
+  {#each tutors as { id, name, title, hourlyRate, first }}
   <Card padding="sm" class="mb-4">
-      <div class="flex justify-end">
-          <DotsHorizontalOutline />
-          <Dropdown class="w-36">
-              <DropdownItem>Edit</DropdownItem>
-              <DropdownItem>Export data</DropdownItem>
-              <DropdownItem>Delete</DropdownItem>
-          </Dropdown>
-      </div>
+
       <div class="flex flex-col items-center pb-4">
-          <Avatar size="lg" src="src\lib\assets\tutors\aims.webp" />
+          <Avatar size="lg" src="src\lib\assets\tutors\{id}\hs.webp" />
           <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name} • ${hourlyRate}/hr</h5>
           <span class="text-sm text-gray-500 dark:text-gray-400 text-center">{title}</span>
           <div class="flex mt-4 space-x-3 lg:mt-6">
-              <Button class="border-solid border-2 border-emerald-400 text-black transform transition-transform duration-500 ease-in-out hover:scale-105">Learn More</Button>
+              <Button href="https://www.tutorsandservices.com/{first}-{id}/" class="border-solid border-2 border-emerald-400 text-black transform transition-transform duration-500 ease-in-out hover:scale-105">Learn More</Button>
           </div>
       </div>
+      
   </Card>
   {/each}
 </div>
--- src/lib/tutors.ts
+++ src/lib/tutors.ts
@@ -8,8 +8,8 @@ export type Tutor = {
     //Personal Info
     id: string;
 
-    // first: string;
-    // last: string;
+    first: string;
+    last: string;
     name: string;
     // email: string;
     // phone: string;
@@ -36,83 +36,109 @@ export type Tutor = {
     // testimonials: string[];
   };
   
-export const tutors: Tutor[] = [
+  export const tutors: Tutor[] = [
     {
         id: '2652',
+        first: 'Tim',
+        last: 'Delaplane',
         name: 'Tim D',
         title: 'SketchUp & Solid WORKS Introductory Tutor & 3D Printing',
         hourlyRate: 40
     },
     {
         id: '12112',
+        first: 'Aims',
+        last: 'Arnhart',
         name: 'Aims A',
         title: 'SketchUp Beginner Tutor and VRAY Rendering Services',
         hourlyRate: 40
     },
     {
         id: '24291',
-        name: 'Vladamir S',
+        first: 'Vladimir',
+        last: 'Simonovski',
+        name: 'Vladimir S',
         title: 'Engineering 3D Visualization and Sketchup Training',
         hourlyRate: 40
     },
     {
         id: '1619',
+        first: 'Shanthi',
+        last: 'OConnell',
         name: 'Shanthi O',
         title: 'Sketchup Interior 3D Modelling and Rendering Services',
         hourlyRate: 45
     },
     {
         id: '12081',
+        first: 'Kyle',
+        last: 'Thoe',
         name: 'Kyle T',
         title: 'Sketchup and Layout Tutor for Beginners to Intermediate',
         hourlyRate: 45
     },
     {
         id: '12113',
+        first: 'Michael',
+        last: 'Hosler',
         name: 'Michael H',
         title: 'AutoCAD Manager, Drafting Specialist and Sketchup Tutor',
         hourlyRate: 45
     },
     {
         id: '18035',
+        first: 'Joshua',
+        last: 'Berindei',
         name: 'Joshua B',
         title: 'Custom 3D Model Drawing Tutor/Service for Architecture &more',
         hourlyRate: 45
     },
     {
         id: '12115',
+        first: 'Juan',
+        last: 'Santizo',
         name: 'Juan S',
         title: '3D Multi-Program & Multidisciplinary Tutor (English/Spanish)',
         hourlyRate: 50
     },
     {
         id: '20684',
+        first: 'Paul',
+        last: 'Hensey',
         name: 'Paul H',
         title: 'Sketchup Trainer for Terrain, Landscape & Garden Design',
         hourlyRate: 75
     },
     {
         id: '2515',
+        first: 'Geoff',
+        last: 'Bostwick',
         name: 'Geoff B',
         title: 'Sketchup Architectural & Custom 3D Modeling Tutor / Services',
         hourlyRate: 80
     },
     {
         id: '2972',
+        first: 'Curt',
+        last: 'Adams',
         name: 'Curt A',
         title: 'Architectural Designer/3D Visualizer/3D Modeling & Rendering',
         hourlyRate: 80
     },
     {
         id: '18836',
+        first: 'Matthew',
+        last: 'Wheeler',
         name: 'Matthew W',
         title: 'Advanced AutoCAD and Sketchup Tutor for Architecture',
         hourlyRate: 100
     },
     {
         id: '22530',
+        first: 'Joanne',
+        last: 'Swisterski',
         name: 'Joanne S',
         title: 'Interior Design Training with Sketchup and Layout',
         hourlyRate: 100
     }
-]
+];
