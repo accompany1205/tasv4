e3b9e6ee85744f9de9caf41463e7674f67bfae57
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-02 15:14:22 -0400

    swiperjs

--- src/lib/components/TutorCard.svelte
+++ src/lib/components/TutorCard.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
-	import { Card, Avatar, Button } from 'flowbite-svelte';
+	import { Card, Button, Toggle } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+
 	import type { Tutor } from '$lib/tutors.ts';
 	export let tutor: Tutor;
-	let _class = '';
-	export { _class as class };
+
+	let vCard = false;
 </script>
+  
+<div>
+	<Card img='/tutors/{tutor.id}/hs.webp' reverse={vCard} class="mb-4">
 
-<Card padding="sm" class="mx-4 mb-0 flex-grow-[1] {_class}">
-	<div class="flex flex-col items-center pb-4">
-		<Avatar size="lg" src='/tutors/{tutor.id}/hs.webp' />
-		<h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{tutor.name} • ${tutor.hourlyRate}/hr</h5>
-		<span class="text-center text-sm text-gray-500 dark:text-gray-400">{tutor.title}</span>
-		<div class="mt-4 flex space-x-3 lg:mt-6">
-			<Button
-				href="https://www.tutorsandservices.com/{tutor.first}-{tutor.id}/"
-				class="transform border-2 border-solid border-emerald-400 text-black transition-transform duration-500 ease-in-out hover:scale-105"
-				>Learn More</Button
+		<swiper-container>
+			<swiper-slide><img src='/tutors/{tutor.id}/hs.webp' alt=""></swiper-slide>
+			<swiper-slide><img src='/tutors/{tutor.id}/hs.webp' alt=""></swiper-slide>
+			<swiper-slide><img src='/tutors/{tutor.id}/hs.webp' alt=""></swiper-slide>
+		</swiper-container>
+
+		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{tutor.name} • ${tutor.hourlyRate}/hr</h5>
+		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{tutor.title}</p>
+		<Button 
+			class="transform border-2 border-solid border-emerald-400 text-black transition-transform duration-250 ease-in-out active:bg-alabaster-50 hover:bg-alabaster-50"
+			href="https://www.tutorsandservices.com/{tutor.first}-{tutor.id}/"
 			>
-		</div>
-	</div>
-</Card>
-
-<!-- src\lib\assets\tutors\1619\hs.webp -->
-<!-- src\lib\components\TutorCard.svelte -->
+			
+			Learn More <ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-black" />
+		</Button>
+	</Card>
+</div>
--- src/lib/components/TutorList.svelte
+++ src/lib/components/TutorList.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-	import TutorCard from '$lib/components/TutorCard.svelte';
+	import TutorCard from './TutorCard.svelte';
 	import { tutors } from '$lib/tutors';
 </script>
 
 <div class="m-auto flex w-full flex-wrap justify-center gap-6">
+	<!-- {#each tutors as tutor}
+		<TutorCard tutor="{tutor}" />
+	{/each} -->
+
 	{#each tutors as tutor}
 		<TutorCard tutor="{tutor}" />
 	{/each}
+
 </div>
