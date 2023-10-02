5590a71d7798610f5d0d6731c9751ff20a81bb92
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-02 15:45:06 -0400

    lint and sliders in cards

--- src/lib/components/TutorCard.svelte
+++ src/lib/components/TutorCard.svelte
@@ -1,3 +1,9 @@
+<style>
+	swiper-container {
+		--swiper-theme-color: #61ce70;
+	}
+</style>
+
 <script lang="ts">
 	import { Card, Button, Toggle } from 'flowbite-svelte';
 	import { ArrowRightOutline } from 'flowbite-svelte-icons';
@@ -7,24 +13,25 @@
 
 	let vCard = false;
 </script>
-  
-<div>
-	<Card img='/tutors/{tutor.id}/hs.webp' reverse={vCard} class="mb-4">
 
-		<swiper-container>
-			<swiper-slide><img src='/tutors/{tutor.id}/hs.webp' alt=""></swiper-slide>
-			<swiper-slide><img src='/tutors/{tutor.id}/hs.webp' alt=""></swiper-slide>
-			<swiper-slide><img src='/tutors/{tutor.id}/hs.webp' alt=""></swiper-slide>
+<div>
+	<Card img="/" reverse="{vCard}" class="mb-4">
+		<swiper-container pagination="true" pagination-dynamic-bullets="true">
+			<swiper-slide><img src="/tutors/{tutor.id}/hs.webp" alt="" /></swiper-slide>
+			<swiper-slide><img src="src/lib/assets/sketchup/sketchup_1.webp" alt="" /></swiper-slide
+			>
+			<swiper-slide><img src="/tutors/{tutor.id}/hs.webp" alt="" /></swiper-slide>
 		</swiper-container>
 
-		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{tutor.name} • ${tutor.hourlyRate}/hr</h5>
-		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{tutor.title}</p>
-		<Button 
-			class="transform border-2 border-solid border-emerald-400 text-black transition-transform duration-250 ease-in-out active:bg-alabaster-50 hover:bg-alabaster-50"
+		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+			>{tutor.name} • ${tutor.hourlyRate}/hr</h5
+		>
+		<p class="mb-3 font-normal leading-tight text-gray-700 dark:text-gray-400">{tutor.title}</p>
+		<Button
+			class="duration-250 transform border-2 border-solid border-emerald-400 text-black transition-transform ease-in-out hover:bg-alabaster-50 active:bg-alabaster-50"
 			href="https://www.tutorsandservices.com/{tutor.first}-{tutor.id}/"
-			>
-			
-			Learn More <ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-black" />
+		>
+			Learn More <ArrowRightOutline class="ml-2 h-3.5 w-3.5 text-black" />
 		</Button>
 	</Card>
 </div>
--- src/lib/components/TutorList.svelte
+++ src/lib/components/TutorList.svelte
@@ -4,12 +4,7 @@
 </script>
 
 <div class="m-auto flex w-full flex-wrap justify-center gap-6">
-	<!-- {#each tutors as tutor}
-		<TutorCard tutor="{tutor}" />
-	{/each} -->
-
 	{#each tutors as tutor}
 		<TutorCard tutor="{tutor}" />
 	{/each}
-
 </div>
