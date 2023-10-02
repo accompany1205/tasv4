da55b10b4116a902f8107031a88e85ce77a5531f
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-02 14:11:03 -0600

    Desktop Optimizations

--- src/lib/blocks/frontpage/GeneralServicesDetail.svelte
+++ src/lib/blocks/frontpage/GeneralServicesDetail.svelte
@@ -2,17 +2,23 @@
     <h3 class="text-3xl  font-black  border-emerald-400 border-b-4 py-4 pl-4">
         Remote & On-Demand Help
     </h3>
-    <h4 class="text-2xl py-4">
-        Sketchup Tutors and Services
-    </h4>
-    <p>
-        Get on demand SketchUp classes!
-        We are a team of tutors and freelancers that are hire-able on an as-needed basis for professional help with your projects.
-        Call us with a quick question, or a big project.
-        Our availability is flexible.
-        We teach remotely, so you can learn from the comfort of your home, from the jobsite, or from the beach!
-        We teach classes via Zoom screenshare, so we can see each others' screens and cursors.
-        It can take hours to search online for 1 simple piece of information, but just minutes to ask your private tutor.
-    </p>
-    <!-- TODO: Insert Slider Gallery Here -->
+    <div class="grid gap-4 sm:grid-cols-2">
+        <div class="max-w-sm">
+            <h4 class="text-2xl py-4">
+                Sketchup Tutors and Services
+            </h4>
+            <p>
+                Get on demand SketchUp classes!
+                We are a team of tutors and freelancers that are hire-able on an as-needed basis for professional help with your projects.
+                Call us with a quick question, or a big project.
+                Our availability is flexible.
+                We teach remotely, so you can learn from the comfort of your home, from the jobsite, or from the beach!
+                We teach classes via Zoom screenshare, so we can see each others' screens and cursors.
+                It can take hours to search online for 1 simple piece of information, but just minutes to ask your private tutor.
+            </p>
+        </div>
+        <div class="slider bg-pink-500 aspect-video">
+            <!-- TODO: Slider -->
+        </div>
+    </div>
 </div>
--- src/lib/components/ServiceCard.svelte
+++ src/lib/components/ServiceCard.svelte
@@ -3,6 +3,11 @@
 	export { _class as class };
 </script>
 
-<div class="card flex-shrink-0 p-4  {_class}">
-    <slot/>
+<div class="card flex-shrink-0 p-4 grid gap-4 sm:grid-cols-2 {_class}">
+    <div class="max-w-sm">
+		<slot/>
+	</div>
+	<div class="slider bg-pink-500 aspect-video">
+		<!-- TODO: Slider -->
+	</div>
 </div>
