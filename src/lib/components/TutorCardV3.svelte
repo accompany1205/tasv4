de7da553fe716c61de29ef1696adb9ce734f5aae
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-17 13:35:38 -0400

    scaffolding out the cards

---
+++ src/lib/components/TutorCardV3.svelte
@@ -0,0 +1,59 @@
+<script>
+    import '@fontsource-variable/akshar';
+</script>
+
+<style>
+</style>
+  
+<div class="bg-white rounded shadow-lg max-w-md	max-h-96 mx-auto font-akshar">
+    <!-- Image -->
+    <div class="w-full">
+        <img src="/temp/sketchup_9.webp" alt="Matthew W" class="">
+    </div>
+
+    <!-- Tutor Info -->
+    <div class="flex">
+        <!-- Headshot -->
+        <div class="w-1/4">
+            <img src="/tutors/18836/hs.webp" alt="Matthew W" class="rounded-full w-32 h-32 border-emerald-400 border-2 p-1">
+        </div>
+
+        <!-- Name -->
+        <div class="w-3/4 p-2">
+            <div class="text-4xl font-bold">Matthew W</div>
+            <div class="text-yellow-300 font-medium">⭐⭐⭐⭐⭐ | 50 Reviews</div>
+            <div class="text-gray-500 font-medium text-xl">Expert • $100/hr</div>
+        </div>
+    </div>
+
+    <!-- Title -->
+    <div class="m-4 p-4 text-center text-xl">Advanced AutoCAD and Sketchup Tutor for Architecture</div>
+
+    <!-- Software -->
+    <div class="m-4 p-4">
+        <div class="font-bold">Software I Provide:</div>
+        <ul class="list-disc pl-5 mt-2 text-gray-500">
+            <li>SketchUp</li>
+            <li>AutoCAD</li>
+            <li>Layout</li>
+        </ul>
+    </div>
+
+    <!-- Services -->
+    <div class="m-4 p-4">
+        <div class="font-bold">Services I Provide:</div>
+        <ul class="list-disc pl-5 mt-2 text-gray-500">
+        <li>Architectural Modeling</li>
+        <li>Building Custom Dynamic Components</li>
+        <li>Architectural Plans</li>
+        <li>Architectural Rendering</li>
+        <li>3D Modeling</li>
+        </ul>
+    </div>
+
+    <!-- Buttons -->
+    <div class="m-4 p-4 flex justify-between">
+        <button class="text-blue-500 border border-blue-500 rounded px-4 py-2">Learn More About Matthew W</button>
+        <button class="bg-green-500 text-white rounded px-4 py-2">Book A Free Consultation</button>
+    </div>
+</div>
---
+++ src/routes/sandbox/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	//Components
+	import TutorCardV3 from "$lib/components/TutorCardV3.svelte";
+
+	import { tutors } from '$lib/tutors';
+
+
+</script>
+
+<svelte:head>
+	<title>Sandbox</title>
+</svelte:head>
+<TutorCardV3/>
