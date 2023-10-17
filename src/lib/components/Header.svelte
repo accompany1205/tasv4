3a15e868037793bfbefc196ad42f2a8cec482ced
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-17 11:42:44 -0600

    Swap header to grid layout, add hamburger

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,14 +2,18 @@
 	import LogoFramed from '$lib/assets/svg/LogoFramed.svelte';
 	import LogoFramedDynamicText from '$lib/assets/svg/LogoFramedDynamicText.svelte';
         import LogoUnframedDynamicText from '$lib/assets/svg/LogoUnframedDynamicText.svelte';
+
+        import { BarsSolid } from 'flowbite-svelte-icons';
 </script>
 
 <header
 	class="
-        flex
+        grid
+        grid-cols-[auto_auto_1fr]
+        gap-4
         h-[50px]
         max-h-16
-        flex-row
+        
         items-center
         justify-between
         overflow-hidden
@@ -22,6 +26,7 @@
         lg:max-h-24
         "
 >
+        <BarsSolid />
 	<LogoUnframedDynamicText
 		fill="black"
 		class="h-[40px] mt-auto max-h-[inherit] font-serif text-lg "
@@ -30,8 +35,8 @@
 		<text class="dyntext" transform="translate(195 76)"> Tutors & </text>
 		<text class="dyntext" transform="translate(195 117)"> Services </text>
 	</LogoUnframedDynamicText>
-	<div class="content"> </div>
-	<div class="call whitespace-nowrap pl-4 sm:pl-9">
+	
+	<div class="call whitespace-nowrap pl-4 sm:pl-9 justify-self-end">
 		<a class="text-xl font-bold text-emerald-500" href="tel: 720-335-5282"> 720-335-5282 </a>
 	</div>
 </header>
