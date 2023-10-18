c8cb13753a67bca49cb7e70fda745011dd041276
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-18 16:12:51 -0400

    nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
+	import HeaderFlowBite from '$lib/components/HeaderFlowBite.svelte';
 
 	import { register } from 'swiper/element/bundle';
 	register();
 </script>
 
-<Header />
+<HeaderFlowBite />
 <slot />
