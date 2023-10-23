3d26e4fde511c26db81253a6d40289c0a90f47b4
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-23 16:38:22 -0400

    all margins and widths are maxed

--- src/lib/components/swiper/TutorV3Swipeblock.svelte
+++ src/lib/components/swiper/TutorV3Swipeblock.svelte
@@ -39,18 +39,20 @@
 	<h2 class="border-b-4 border-emerald-400 max-w-screen-xl m-auto pb-4 px-4 mb-8 text-2xl font-bold text-white sm:text-5xl sm:fluid-h1">
 		Our SketchUp Tutors & Freelancers
 	</h2>
-	<div class="mx-auto h-[625px] max-w-[1920px]" style="container-type: size;">
+
+	<div class="mx-auto h-[625px] max-w-full m-auto xl:max-w-screen-xl " style="container-type: size;">
 		<swiper-container bind:this="{swiper}" init="false" navigation="true">
 			{#each tutors as tutor}
-				<swiper-slide>
-					<div class="mx-4 h-[100cqh] w-auto flex-shrink-0">
+				<swiper-slide class="">
+					<div class="h-[100cqh] p-2 max-w-2xl w-96 m-auto flex-shrink-0 ">
 						<TutorCardV3
 							tutor="{tutor}"
-							class="mx-auto h-full min-h-[600px] max-w-sm self-stretch"
+							class="mx-auto h-full self-stretch"
 						/>
 					</div>
 				</swiper-slide>
 			{/each}
 		</swiper-container>
 	</div>
+
 </div>
