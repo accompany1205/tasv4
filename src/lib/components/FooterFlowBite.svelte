1d4dc9dd9d4bc16c9322445a4d7259574c88f945
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-23 10:47:35 -0400

    faq

--- src/lib/components/FooterFlowBite.svelte
+++ src/lib/components/FooterFlowBite.svelte
@@ -88,10 +88,5 @@
           <GithubSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
         </FooterIcon>
       </div>
-
-      <FreeConsultationSticky/>
     </div>
-
-    
-
   </Footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,4 +13,5 @@
 
 <Header />
 <slot />
+<FreeConsultationSticky/>
 <FooterFlowBite />
