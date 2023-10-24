36895d0561d47d870834a8a12990ae46ea7f3827
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-24 15:52:46 -0400

    Update +page.svelte

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -2,8 +2,9 @@
 	import { browser } from "$app/environment";
 </script>
 
-<svelte:head>
-  {#if browser}
-    <script type="text/javascript" src="https://form.jotform.com/jsform/213494772938167"></script>
-  {/if}
-</svelte:head>
+
+{#if browser}
+  <script async type="text/javascript" src="https://form.jotform.com/jsform/213494772938167"></script>
+{/if}
+
+  
