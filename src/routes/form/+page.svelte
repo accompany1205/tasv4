4d46a075c4e6841e9945029e6944055fdadefed8
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-24 15:57:58 -0400

    Update +page.svelte

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,10 +1,19 @@
-<script>
+<script lang="ts">
 	import { browser } from "$app/environment";
+  export const ssr = false;
+  import { onMount } from 'svelte';
+  
+  onMount(async () => { 
+    let script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://form.jotform.com/jsform/213494772938167';
+      document.getElementById("jotform").appendChild(script);
+  });
 </script>
 
+<div id="jotform">
+</div>
 
-{#if browser}
-  <script async type="text/javascript" src="https://form.jotform.com/jsform/213494772938167"></script>
-{/if}
+
 
   
