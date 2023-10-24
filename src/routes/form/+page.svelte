36e193b524cf9aac61623e98f1ddb86eb8726ff9
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-24 15:33:58 -0400

    Create +page.svelte

---
+++ src/routes/form/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { browser } from "$app/environment";
+</script>
+
+<svelte:head>
+  {#if browser}
+    <script type="text/javascript" src="https://form.jotform.com/jsform/213494772938167"></script>
+  {/if}
+</svelte:head>
