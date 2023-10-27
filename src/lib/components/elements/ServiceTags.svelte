f02f19b554e4ee9106770ef510b2fdb083d74bc3
accompany1205 <accompaniedwithyou@gmail.com>
2023-10-27 13:22:11 -0400

    Cleaning up services

--- src/lib/components/elements/ServiceTags.svelte
+++ src/lib/components/elements/ServiceTags.svelte
@@ -10,7 +10,7 @@
 
 <div>
 	{#each keywords as keyword}
-		<span class="m-1 inline-block rounded-md bg-slate-100 px-2 py-1 text-sm font-medium"
+		<span class="m-1 inline-block rounded-md bg-slate-100 px-2 py-1 text-sm font-medium text-gray-950"
 			>{keyword}</span
 		>
 	{/each}
