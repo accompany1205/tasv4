2bf134131ca259043b66a1ead1f75a04de217cde
accompany1205 <accompaniedwithyou@gmail.com>
2023-11-01 16:59:53 -0400

    quick fix

--- src/lib/components/blocks/common/FooterFlowBite.svelte
+++ src/lib/components/blocks/common/FooterFlowBite.svelte
@@ -69,15 +69,15 @@
                     <h2 class="my-6  text-2xl font-bold text-white">We Provide</h2>
                     <div class="grid grid-cols-1 text-base">
                         <FooterLinkGroup ulClass="list-disc list-inside">
-                            <FooterLink liClass="mb-1">Professional services</FooterLink>
-                            <FooterLink liClass="mb-1">Private & flexible tutors</FooterLink>
-                            <FooterLink liClass="mb-1">Personalized lessons</FooterLink>
-                            <FooterLink liClass="mb-1">Pro online training</FooterLink>
-                            <FooterLink liClass="mb-1">Help for all abilities</FooterLink>
-                            <FooterLink liClass="mb-1">Service for all budgets</FooterLink>
-                            <FooterLink liClass="mb-1">Screen-share teachers</FooterLink>
-                            <FooterLink liClass="mb-1">Custom Zoom classes</FooterLink>
-                            <FooterLink liClass="mb-1">1-on-1 Software Help</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Professional services</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Private & flexible tutors</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Personalized lessons</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Pro online training</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Help for all abilities</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Service for all budgets</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Screen-share teachers</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Custom Zoom classes</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">1-on-1 Software Help</FooterLink>
                         </FooterLinkGroup>
                     </div>
                 </div>
@@ -86,15 +86,15 @@
                     <h2 class="my-6 text-2xl font-bold text-white">Top Classes</h2>
                     <div class="grid grid-cols-1 text-base">
 						<FooterLinkGroup ulClass="list-disc list-inside">
-							<FooterLink liClass="mb-1">AutoCAD</FooterLink>
-							<FooterLink liClass="mb-1">Blender</FooterLink>
-							<FooterLink liClass="mb-1">Cinema 4D</FooterLink>
-							<FooterLink liClass="mb-1">Fusion 360</FooterLink>
-							<FooterLink liClass="mb-1">Revit</FooterLink>
-							<FooterLink liClass="mb-1">Rhino</FooterLink>
-							<FooterLink liClass="mb-1">Sketchup</FooterLink>
-							<FooterLink liClass="mb-1">Solidworks</FooterLink>
-							<FooterLink liClass="mb-1">Unreal Engine</FooterLink>
+							<FooterLink href="#" liClass="mb-1">AutoCAD</FooterLink>
+							<FooterLink href="#" liClass="mb-1">Blender</FooterLink>
+							<FooterLink href="#" liClass="mb-1">Cinema 4D</FooterLink>
+							<FooterLink href="#" liClass="mb-1">Fusion 360</FooterLink>
+							<FooterLink href="#" liClass="mb-1">Revit</FooterLink>
+							<FooterLink href="#" liClass="mb-1">Rhino</FooterLink>
+							<FooterLink href="#" liClass="mb-1">Sketchup</FooterLink>
+							<FooterLink href="#" liClass="mb-1">Solidworks</FooterLink>
+							<FooterLink href="#" liClass="mb-1">Unreal Engine</FooterLink>
 						</FooterLinkGroup>
                     </div>
                 </div>
@@ -103,15 +103,15 @@
                     <h2 class="my-6 text-2xl font-bold text-white">Top Services</h2>
                     <div class="grid grid-cols-1 text-base">
                         <FooterLinkGroup ulClass="list-disc list-inside">
-                            <FooterLink liClass="mb-1">3D Print Modeling</FooterLink>
-                            <FooterLink liClass="mb-1">Architectural Rendering</FooterLink>
-                            <FooterLink liClass="mb-1">Construction Documentation</FooterLink>
-                            <FooterLink liClass="mb-1">Furniture Modeling</FooterLink>
-                            <FooterLink liClass="mb-1">Interior Modeling</FooterLink>
-                            <FooterLink liClass="mb-1">Interior Rendering</FooterLink>
-                            <FooterLink liClass="mb-1">Mechanical Modeling</FooterLink>
-                            <FooterLink liClass="mb-1">Product Rendering</FooterLink>
-                            <FooterLink liClass="mb-1">Virtual Tours</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">3D Print Modeling</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Architectural Rendering</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Construction Documentation</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Furniture Modeling</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Interior Modeling</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Interior Rendering</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Mechanical Modeling</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Product Rendering</FooterLink>
+                            <FooterLink href="#" liClass="mb-1">Virtual Tours</FooterLink>
                         </FooterLinkGroup>
                     </div>
                 </div>
