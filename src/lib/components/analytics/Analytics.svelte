65c6a73ce8ca2b1705031325d5840797a9f7b47a
accompany1205 <accompaniedwithyou@gmail.com>
2023-11-09 17:45:22 -0500

    adding google ads tag

--- src/lib/components/analytics/Analytics.svelte
+++ src/lib/components/analytics/Analytics.svelte
@@ -9,16 +9,34 @@
         })
       }
     }
-  </script>
+
+    $: {
+      if (typeof gtag !== 'undefined') {
+        gtag('config', 'AW-11398048179', {
+          page_title: document.title,
+          page_path: $page.url.pathname,
+        })
+      }
+    }
+</script>
   
 <svelte:head>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-C6W85YZ4Q5"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-C6W85YZ4Q5"></script>
+  <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-        gtag('config', 'G-C6W85YZ4Q5');
-    </script>
+      gtag('config', 'G-C6W85YZ4Q5');
+  </script>
+
+  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11398048179"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-11398048179');
+  </script>
 </svelte:head>
   
