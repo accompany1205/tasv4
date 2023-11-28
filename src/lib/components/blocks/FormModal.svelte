6ea531cfd9fab317f0f66fe7b461a5315ea75b1e
accompany1205 <accompaniedwithyou@gmail.com>
2023-11-28 15:22:02 -0500

    Form

---
+++ src/lib/components/blocks/FormModal.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Button, Modal } from 'flowbite-svelte';
+    let clickOutsideModal = false;
+</script>
+  
+<Button
+  class="max-w-sm items-center bg-emerald-400 p-3 text-center text-3xl font-bold text-white hover:bg-emerald-300"
+  href="/form?softwarePref=SketchUp"
+    >
+  Get Started Today
+</Button>
+  
+<Modal title="Terms of Service" bind:open={clickOutsideModal} autoclose outsideclose>
+
+</Modal>
