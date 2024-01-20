3b1268548c0e8f2e30abe0b1954861e8323fd1aa
accompany1205 <accompaniedwithyou@gmail.com>
2024-01-20 14:30:30 +0530

    Added horizontal line separator

--- src/lib/components/blocks/frontpage/SketchupServices.svelte
+++ src/lib/components/blocks/frontpage/SketchupServices.svelte
@@ -1,48 +1,65 @@
 <script lang="ts">
+	import { Hr } from 'flowbite-svelte';
 	import ServiceCard from '$lib/components/cards/ServiceCard.svelte';
 	import Section2Col from '$lib/components/elements/Section2Col.svelte';
 	import H3 from '$lib/components/elements/H3.svelte';
 	import H4 from '$lib/components/elements/H4.svelte';
-	import P from '$lib/components/elements/P.svelte'; 
+	import P from '$lib/components/elements/P.svelte';
 	import ImageCarousel from '$lib/components/cards/ImageCarousel.svelte';
 	import services from '$lib/services';
-	const concept_arc_import: Record<string, string> = import.meta.glob('$lib/assets/sketchup/services/conceptual-architecture/*.webp', {
-		query: {
-			format: 'webp;jpeg',
-			w: '512;256;128;', //120;240;480;
-			picture: '',
-			as: 'srcset',
+	const concept_arc_import: Record<string, string> = import.meta.glob(
+		'$lib/assets/sketchup/services/conceptual-architecture/*.webp',
+		{
+			query: {
+				format: 'webp;jpeg',
+				w: '512;256;128;', //120;240;480;
+				picture: '',
+				as: 'srcset',
+			},
+			import: 'default',
+			eager: true,
 		},
-		import: 'default',
-		eager: true,
-	});
-	const concept_arc_flattened = Object.values(concept_arc_import).map((url) => ({alt:'', srcset:url}));
+	);
+	const concept_arc_flattened = Object.values(concept_arc_import).map((url) => ({
+		alt: '',
+		srcset: url,
+	}));
 
-	const interior_design_import: Record<string, string> = import.meta.glob('$lib/assets/sketchup/services/interior-design/*.webp', {
-		query: {
-			format: 'webp;jpeg',
-			w: '512;256;128;', //120;240;480;
-			picture: '',
-			as: 'srcset',
+	const interior_design_import: Record<string, string> = import.meta.glob(
+		'$lib/assets/sketchup/services/interior-design/*.webp',
+		{
+			query: {
+				format: 'webp;jpeg',
+				w: '512;256;128;', //120;240;480;
+				picture: '',
+				as: 'srcset',
+			},
+			import: 'default',
+			eager: true,
 		},
-		import: 'default',
-		eager: true,
-	}); 
-	const interior_design_flattened = Object.values(interior_design_import).map((url) => ({alt:'', srcset:url}));
+	);
+	const interior_design_flattened = Object.values(interior_design_import).map((url) => ({
+		alt: '',
+		srcset: url,
+	}));
 
-
-	const product_design_import: Record<string, string> = import.meta.glob('$lib/assets/sketchup/services/product-design/*.webp', {
-		query: {
-			format: 'webp;jpeg',
-			w: '512;256;128;', //120;240;480;
-			picture: '',
-			as: 'srcset',
+	const product_design_import: Record<string, string> = import.meta.glob(
+		'$lib/assets/sketchup/services/product-design/*.webp',
+		{
+			query: {
+				format: 'webp;jpeg',
+				w: '512;256;128;', //120;240;480;
+				picture: '',
+				as: 'srcset',
+			},
+			import: 'default',
+			eager: true,
 		},
-		import: 'default',
-		eager: true,
-	});
-	const product_design_flattened = Object.values(product_design_import).map((url) => ({alt:'', srcset:url}));
-
+	);
+	const product_design_flattened = Object.values(product_design_import).map((url) => ({
+		alt: '',
+		srcset: url,
+	}));
 </script>
 
 <div class="bg-nile-blue-900 p-4 text-white">
@@ -55,6 +72,7 @@
 			expensive fees for redrawing or changing plans.
 		</P>
 	</ServiceCard>
+	<Hr classHr="m-0 mb-14 mx-auto max-w-7xl" />
 
 	<ServiceCard images="{interior_design_flattened}">
 		<H4>Interior Design</H4>
@@ -64,6 +82,7 @@
 			customizable. There are no limitations to what you can create.
 		</P>
 	</ServiceCard>
+	<Hr classHr="m-0 mb-14 mx-auto max-w-7xl" />
 
 	<ServiceCard images="{product_design_flattened}">
 		<H4>Product Design</H4>
@@ -72,6 +91,7 @@
 			conduct rapid prototyping.
 		</P>
 	</ServiceCard>
+	<Hr classHr="m-0 mb-14 mx-auto max-w-7xl" />
 
 	<div class="card mx-auto grid max-w-7xl flex-shrink-0 gap-4 p-4 sm:grid-cols-2">
 		<div class="order-1 sm:-order-1">
