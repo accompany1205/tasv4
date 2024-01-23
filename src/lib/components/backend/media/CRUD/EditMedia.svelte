06f5e0dad419baf59dbf740b2350676f1ab6ca0d
accompany1205 <accompaniedwithyou@gmail.com>
2024-01-23 11:47:46 -0500

    Update EditMedia.svelte

--- src/lib/components/backend/media/CRUD/EditMedia.svelte
+++ src/lib/components/backend/media/CRUD/EditMedia.svelte
@@ -3,7 +3,12 @@
     import { Label, Input } from 'flowbite-svelte';
 
     export let image: any;
-    let defaultModal = false;
+    let defaultModal = true;
+
+    let title = image.title;
+    let altText = image.alt;
+    let published = image.published;
+    let tags = image.tags;    
 </script>
 
 <Button color="alternative" on:click={() => (defaultModal = true)}>Edit</Button>
@@ -16,45 +21,49 @@
         </div>
 
         <div class="grid gap-4 sm:grid-cols-1 px-10 w-1/2 overflow-y-auto">
+
+            <!-- Editable -->
             <div class="mb-6">
-                <Label for="input-group-1" class="block mb-2">Your Email</Label>
-                <Input id="email" type="email" placeholder="name@flowbite.com"/>
+                <Label for="input-group-1" class="block mb-2">Title</Label>
+                <Input id="title" type="text" bind:value={title} />
             </div>
 
             <div class="mb-6">
-                <Label for="input-group-1" class="block mb-2">Your Email</Label>
-                <Input id="email" type="email" placeholder="name@flowbite.com"/>
+                <Label for="input-group-1" class="block mb-2">Alt</Label>
+                <Input id="altText" type="text" bind:value={altText} />
             </div>
 
             <div class="mb-6">
-                <Label for="input-group-1" class="block mb-2">Your Email</Label>
-                <Input id="email" type="email" placeholder="name@flowbite.com"/>
-            </div>
-            <div class="mb-6">
-                <Label for="input-group-1" class="block mb-2">Your Email</Label>
-                <Input id="email" type="email" placeholder="name@flowbite.com"/>
-            </div>
-
-
-            <div class="mb-6">
-                <Label for="input-group-1" class="block mb-2">Your Email</Label>
-                <Input id="email" type="email" placeholder="name@flowbite.com"/>
+                <Label for="input-group-1" class="block mb-2">Published</Label>
+                <Input id="published" type="text" bind:value={published} />
             </div>
 
             <div class="mb-6">
-                <Label for="input-group-1" class="block mb-2">Your Email</Label>
-                <Input id="email" type="email" placeholder="name@flowbite.com"/>
+                <Label for="input-group-1" class="block mb-2">Tags</Label>
+                <Input id="tags" type="text" bind:value={tags} />
+            </div>
+
+
+            <!-- Static -->
+            <div class="mb-6">
+                <Label for="input-group-1" class="block mb-2">File Name</Label>
+                <Input id="name" type="text" value={image.name}/>
+            </div>
+
+            <div class="mb-6">
+                <Label for="input-group-1" class="block mb-2">Owner</Label>
+                <Input id="owner" type="text" value={image.owner}/>
             </div>
 
 
             <div class="mb-6">
-                <Label for="input-group-1" class="block mb-2">Your Email</Label>
-                <Input id="email" type="email" placeholder="name@flowbite.com"/>
+                <Label for="input-group-1" class="block mb-2">Uploaded</Label>
+                <Input id="uploadDate" type="date" value={image.uploadDate}/>
             </div>
 
             <div class="mb-6">
-                <Label for="input-group-1" class="block mb-2">Your Email</Label>
-                <Input id="email" type="email" placeholder="name@flowbite.com"/>
+                <Label for="input-group-1" class="block mb-2">Url</Label>
+                <Input id="url" type="text" value={image.url}/>
             </div>
 
         </div>
