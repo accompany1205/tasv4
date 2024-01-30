c1129868ae85d26388f89f02692231af55beb121
accompany1205 <accompaniedwithyou@gmail.com>
2024-01-30 13:28:44 -0500

    reorder be gucci

--- src/lib/components/backend/services/CRUD/page-sections/edit-sub-services/EditSub.svelte
+++ src/lib/components/backend/services/CRUD/page-sections/edit-sub-services/EditSub.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Button, Label, Textarea, Popover  } from 'flowbite-svelte';
+    import { Button, Label, Textarea, Popover, ButtonGroup  } from 'flowbite-svelte';
     import { writable } from 'svelte/store';
     import { doc, updateDoc } from 'firebase/firestore';
     import { db } from '$lib/firebase';
@@ -32,39 +32,86 @@
         }
     }
 
+    async function deleteSubService(index: number) {
+        subServices.update(currentSubServices => {
+            return currentSubServices.filter((_, i) => i !== index);
+        });
+
+        updateSubServices();
+    }
+
     subServices.subscribe(() => {
         updateSubServices();
     });
+
+    let draggedItem: number | null = null;
+
+
+function handleDragStart(event: DragEvent & { currentTarget: EventTarget & HTMLDivElement; }, index: number | null) {
+    draggedItem = index;
+    event.dataTransfer.effectAllowed = 'move';
+}
+
+function handleDragOver(event: DragEvent & { currentTarget: EventTarget & HTMLDivElement; }, index: number) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+}
+
+function handleDrop(event: DragEvent & { currentTarget: EventTarget & HTMLDivElement; }, index: number) {
+    event.preventDefault();
+
+    if (draggedItem === null || draggedItem === index) {
+        return;
+    }
+
+    subServices.update(currentSubServices => {
+        const draggedSubService = currentSubServices[draggedItem];
+        let newSubServices = currentSubServices.filter((_, i) => i !== draggedItem);
+        newSubServices.splice(index, 0, draggedSubService);
+        return newSubServices;
+    });
+
+    draggedItem = null;
+    updateSubServices();
+}
+
+
+
 </script>
 
 {#each $subServices as subService, i}
-    <div class="flex gap-10 mb-20">
-        <Label class="mb-2 text-sm w-2/12">Sub-service Name
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div 
+        class="flex gap-10 mb-20 text-gray-800 font-semibold"
+        draggable="true"
+        on:dragstart={(event) => handleDragStart(event, i)}
+        on:dragover={(event) => handleDragOver(event, i)}
+        on:drop={(event) => handleDrop(event, i)}
+    >
+        <div class="mb-2 text-sm w-2/12">Sub-service Name
             <Textarea bind:value={subService.name} class="mt-2 h-auto" placeholder="SubService Name"/>
-        </Label>
+        </div>
 
-        <Label class="mb-2 text-sm w-3/12">SubService Description
+        <div class="mb-2 text-sm w-3/12">SubService Description
             <Textarea bind:value={subService.description} class="mt-2 " placeholder="SubService Description" rows="5" />
-        </Label>
+        </div>
 
-        <Label class="mb-2 text-sm w-6/12">Images
+        <div class="mb-2 text-sm w-6/12">Images
             <Images subService={subService} serviceId={serviceId} subServiceIndex={i} divClass="mt-2"/>
-        </Label>
+        </div>
 
+        <div class="flex flex-col">
+            <div class="mb-2 text-sm">Actions</div>
 
-        <Label class="mb-2 text-sm">Actions
-            <div class="mt-2 flex flex-col gap-4">
-                <Button class="h-fit w-4" color="alternative">
-                    <BarsSolid/>
-                </Button>
+            <Button color="alternative" class="mb-2">
+                <BarsSolid/>
+            </Button>
 
-                <Button class="h-fit w-4" color="alternative">
-                    <TrashBinOutline/>
-                </Button>      
-            </div>
-        </Label>
-
-
+            <Button color="alternative" on:click={() => deleteSubService(i)}>
+                <TrashBinOutline/>
+            </Button>      
+        </div>
+        
     </div>
 {/each}
 
